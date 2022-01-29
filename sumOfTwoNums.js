a5aca7ccb6aea220263c5f9a5c3e8c474d965b6a
bradripple <bradripple@gmail.com>
2022-01-29 13:25:19 -0700

    Created sumOfTwoNums with problem solving logic

---
+++ sumOfTwoNums.js
@@ -0,0 +1,23 @@
+// Write a function which takes two numbers and returns their sum.
+
+// 1. Can I restate the problem in my own words?
+"add two numbers together"
+
+// 2. What are the inputs that go into the problems?
+// - ints?
+// - floats?
+// - what about string for large numbers?
+
+// 3. What are the outputs that should come from the solution to the problem?
+// - int? float? string?
+
+// 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+
+// 5. How should I label the important pieces of data that are a part of the problem?
+
+function sumOfTwoNums(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+console.log(sumOfTwoNums(5, 45));
