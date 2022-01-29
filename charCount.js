eef27ef2ab83eada9352c086d8d231504f426409
bradripple <bradripple@gmail.com>
2022-01-29 14:44:11 -0700

    Completed charCount with refactored solution

--- charCount.js
+++ charCount.js
@@ -53,19 +53,53 @@
 //     return result;
 // }
 
+// function charCount(str) {
+//     let obj = {};
+//     for (let i = 0; i < str.length; i++) {
+//         let char = str[i].toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             if (obj[char] > 0) {
+//                 obj[char]++;
+//             } else {
+//                 obj[char] = 1;
+//             }
+//         }
+//     }
+//     return obj;
+// }
+
+// final refactored solution
+// function charCount(str) {
+//     let obj = {};
+//     for (let char of str) {
+//         char = char.toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             obj[char] = obj[char]++ || 1;
+//         }
+//     }
+//     return obj;
+// }
+
 function charCount(str) {
     let obj = {};
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i].toLowerCase();
-        if (/[a-z0-9]/.test(char)) {
-            if (obj[char] > 0) {
-                obj[char]++;
-            } else {
-                obj[char] = 1;
-            }
+    for (let char of str) {
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase();
+            obj[char] = obj[char]++ || 1;
         }
     }
     return obj;
 }
 
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)) {
+            return false;
+        }
+        return true;
+}
+
 console.log(charCount("Hi there!"));
+
