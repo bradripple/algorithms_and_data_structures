7914c3d1e57744e4297a2718dc7bc4da339997cf
bradripple <bradripple@gmail.com>
2022-02-03 11:48:37 -0700

    Created validAnagram using a frequency counter pattern

---
+++ validAnagram.js
@@ -0,0 +1,77 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of antoher, such as cinema, formed from iceman
+
+
+// 1. Can I restate the problem in my own words?
+"count the characters that are included in the string"
+
+// 2. What are the inputs that go into the problems?
+// Should I account for spaces? Capitalized letters? Numbers? Special characters?
+"only alphanumric and lowercase characters"
+
+// 3. What are the outputs that should come from the solution to the problem?
+
+// charCount("aaaa"); // {a:4}
+// charCount("hello"); // {h:1, e:1, l:2, o:1}
+
+// "my phone number is 8082389988"
+// "Hello Hi"
+// charCount("")
+
+// 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+
+// 5. How should I label the important pieces of data that are a part of the problem?
+
+
+// function validAnagram(first, second) {
+//     if(first.length !== second.length) {
+//         return false;
+//     }
+
+//     let lookup = {};
+//     for(let i = 0; i < first.length; i++) {
+//         let letter = first[i];
+//         // if letter exists, increment, otherwise set to 1
+//         lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+//     }
+//     console.log('first lookup', lookup);
+    
+//     for(let i = 0; i < second.length; i++) {
+//         let letter = second[i];
+//         // can't find letter or letter is zero then it's not an anagram
+//         if(!lookup[letter]) {
+//             return false;
+//         } else {
+//             lookup[letter] -= 1;
+//         }
+//     }
+//     console.log('second lookup', lookup);
+
+//     return true;
+// }
+
+
+function validAnagram(first, second) {
+    if(first.length !== second.length) {
+        return false;
+    }
+
+    lookup = {};
+    for(let i = 0; i < first.length; i++){
+        letter = first[i];
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    console.log('lookup1', lookup)
+
+    for(let i = 0; i < second.length; i++) {
+        letter = second[i];
+        if(!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true;
+}
+
+
+console.log(validAnagram('anagrams', 'nagarams'))
